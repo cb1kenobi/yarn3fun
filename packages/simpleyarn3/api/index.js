18a2c216a2520feaceb5ea16846c45bcf73997ed
Chris Barber <chris.barber@vercel.com>
2023-02-16 16:08:44 -0600

    Fixed api

--- packages/simpleyarn3/api/index.js
+++ packages/simpleyarn3/api/index.js
@@ -1,10 +1,3 @@
-import truetrue from 'true';
-
 export default async function handler(req, res) {
-	if (truetrue) {
-		res.status(200).end(`Definitely true // ${new Date().toISOString()}\n`);
-		return;
-	}
-
 	res.status(200).end(`API KEY = ${process.env.API_KEY || 'No API key set'} // ${new Date().toISOString()}\n`);
 }
